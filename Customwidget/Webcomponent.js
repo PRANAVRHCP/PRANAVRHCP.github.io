c2f24258842012d4f15a296dd80666f08823e629
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2022-11-27 08:06:53 +0100

    Add files via upload

---
+++ Customwidget/Webcomponent.js
@@ -0,0 +1,38 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = 
+    `<button type="button" id="myBtn"> Perf. Help </button>    
+    <script>
+    document.getElementById("id").onclick = function() {myFunction()};
+    function myFunction() {
+      document.getElementById("demo").innerHTML = "YOU CLICKED ME!";}
+    </script>`;
+   
+    class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
+
+        init() {            
+               this.appendChild(tmpl.content.cloneNode(true)); 
+            //const element = this.document.getElementById("myBtn");
+            //element.addEventListener("click", handleButtonClick).bind(this);
+           // this.buttonBoundListener = this.handleButtonClick.bind(this);
+        }
+
+        fireChanged() {
+            console.log("OnClick Triggered");            
+        }
+        
+        handleButtonClick() {        
+        console.log('Here');
+    }
+
+        clear() {
+            console.log("");
+        }       
+    }
+
+    customElements.define('pka-button', PerformanceHelp);
+})();
