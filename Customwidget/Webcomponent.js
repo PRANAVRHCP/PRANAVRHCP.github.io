fc650bf6bde0a277f3cd2721fee30d1dcb14d792
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2022-11-28 11:02:44 +0100

    Update Webcomponent.js

--- Customwidget/Webcomponent.js
+++ Customwidget/Webcomponent.js
@@ -22,7 +22,16 @@
 
         fireChanged() {
             console.log("OnClick Triggered");      
-            var measures = window.sap.raptr.getEntries().filter(e => e.entryType === 'measure')            ;
+            var measures = window.sap.raptr.getEntries();
+            var exportObj = 'RaptrMeasures.json';
+            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+            var downloadAnchorNode = document.createElement('a');
+            downloadAnchorNode.setAttribute("href", dataStr);
+            downloadAnchorNode.setAttribute("download", exportName);
+            document.body.appendChild(downloadAnchorNode); // required for firefox
+            downloadAnchorNode.click();
+            downloadAnchorNode.remove();
+
             console.log(measures);
         }        
         
