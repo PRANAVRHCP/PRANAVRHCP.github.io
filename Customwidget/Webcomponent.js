ba1132c2302a53be48afae1bbc3fd03241761aba
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2022-12-07 15:15:38 +0100

    Update Webcomponent.js

--- Customwidget/Webcomponent.js
+++ Customwidget/Webcomponent.js
@@ -118,7 +118,7 @@
       {
         let stepStartTime = 0;
         let stepEndTime = 0;
-        let maxEndTime = 0;
+        let maxEndTime = x[0].stepDuration;
         let endTime = 0;
         let stepNo = 1;
         var maxstepid = 0;
