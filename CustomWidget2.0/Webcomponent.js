041f323c17dbca6fb23ca441a5a96085eb46367a
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2022-12-14 13:56:32 +0100

    Update Webcomponent.js

--- CustomWidget2.0/Webcomponent.js
+++ CustomWidget2.0/Webcomponent.js
@@ -46,7 +46,7 @@
       var x = [];
       var currentStepTime = 0;
       var previousStepTime = 0;
-      previousStepTime = result[result.length-1].startTime;
+      previousStepTime = result[initval-1].startTime;
       //Generate list of steps
       for (var i = initval ; i< result.length ; i++)
               {
