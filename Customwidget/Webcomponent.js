33a3847d79431a0401099f57b2b6e5b748d33d26
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2023-01-02 14:36:17 +0100

    Update Webcomponent.js

--- Customwidget/Webcomponent.js
+++ Customwidget/Webcomponent.js
@@ -7,9 +7,10 @@
       constructor() {
           super();
           // declare global variables to be used across the whole scope of this code
-         window.initval = 0;;
-          window.x = [];
-          this.init();           
+         window.initval = 0;
+         window.result_xhr = [];
+         window.x = [];
+         this.init();           
       }
 
       init() {            
@@ -27,82 +28,9 @@
           });           
       }
 
-    tracknetworkcalls()
-      {
-        this.interceptNetworkRequests({
-          onFetch: console.log,
-          onFetchResponse: console.log,
-          onFetchLoad: console.log,    
-          onOpen: console.log,
-          onSend: console.log,
-          onError: console.log,
-          onLoad: console.log
-      });
-      }
-    
-      interceptNetworkRequests(ee) {
-        const open = XMLHttpRequest.prototype.open;
-        const send = XMLHttpRequest.prototype.send;
-    
-        const isRegularXHR = open.toString().indexOf('native code') !== -1;
-    
-    
-        // don't hijack if already hijacked - this will mess up with frameworks like Angular with zones
-        // we work if we load first there which we can.
-        if (isRegularXHR) {
-            XMLHttpRequest.prototype.open = function() {
-                ee.onOpen && ee.onOpen(this, arguments);
-                if (ee.onLoad) {
-                    this.addEventListener('load', ee.onLoad.bind(ee));
-                }
-                if (ee.onError) {
-                    this.addEventListener('error', ee.onError.bind(ee));
-                }
-                return open.apply(this, arguments);
-            };
-            XMLHttpRequest.prototype.send = function() {
-                ee.onSend && ee.onSend(this, arguments);
-                return send.apply(this, arguments);
-            };
-        }
-    
-        const fetch = window.fetch || "";
-        // don't hijack twice, if fetch is built with XHR no need to decorate, if already hijacked
-        // then this is dangerous and we opt out
-        const isFetchNative = fetch.toString().indexOf('native code') !== -1;
-        if(isFetchNative) {
-            window.fetch = function () {
-                ee.onFetch && ee.onFetch(arguments);
-                const p = fetch.apply(this, arguments);
-                p.then(ee.onFetchResponse, ee.onFetchError);
-                return p;
-            };
-            // at the moment, we don't listen to streams which are likely video 
-            const json = Response.prototype.json;
-            const text = Response.prototype.text;
-            const blob = Response.prototype.blob;
-            Response.prototype.json = function () {
-                const p = json.apply(this.arguments);
-                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "json"));
-                return p;
-            };
-            Response.prototype.text = function () {
-                const p = text.apply(this.arguments);
-                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "text"));
-                return p;
-            };
-            Response.prototype.blob = function () {
-                const p = blob.apply(this.arguments);
-                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "blob"));
-                return p;
-            };
-        }
-        return ee;
-    }  
-
     capturechange()
     {
-      console.log()
+      console.log(result_xhr);
     }
     fireChanged() {
           
@@ -269,4 +197,46 @@
   }
 
   customElements.define('pka-button', PerformanceHelp);
+  // This is only good for logging.
+function addXMLRequestCallback(callback){
+  let oldSend;
+  let i;
+  if( XMLHttpRequest.callbacks ) {
+      // we've already overridden send() so just add the callback
+      XMLHttpRequest.callbacks.push( callback );
+  } else {
+      // create a callback queue
+      XMLHttpRequest.callbacks = [callback];
+      // store the native send()
+      oldSend = XMLHttpRequest.prototype.send;
+      // override the native send()
+      XMLHttpRequest.prototype.send = function(){
+          // process the callback queue
+          // the xhr instance is passed into each callback but seems pretty useless
+          // you can't tell what its destination is or call abort() without an error
+          // so only really good for logging that a request has happened
+          // I could be wrong, I hope so...
+          // EDIT: I suppose you could override the onreadystatechange handler though
+          for( i = 0; i < XMLHttpRequest.callbacks.length; i++ ) {
+              XMLHttpRequest.callbacks[i]( this );
+          }
+          // call the native send()
+          oldSend.apply(this, arguments);
+      }
+  }
+}
+
+        // e.g.
+        addXMLRequestCallback( xhr => {
+         // console.log( xhr.responseText ); // (an empty string)
+         if(xhr.responseText !== '' || xhr.responseText !== undefined )
+         {
+          console.log(xhr.responseText)
+         }
+        });
+        addXMLRequestCallback( xhr => {
+        //  console.dir( xhr ); // have a look if there is anything useful here
+        result_xhr.push(xhr);
+        });
+
 })();
