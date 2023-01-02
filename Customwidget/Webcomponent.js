51ddc2c9a6f96eb7b3fd9071d3483cfb4bdac077
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2023-01-02 14:52:09 +0100

    Update Webcomponent.js

--- Customwidget/Webcomponent.js
+++ Customwidget/Webcomponent.js
@@ -208,13 +208,7 @@ function addXMLRequestCallback(callback){
       // store the native send()
       oldSend = XMLHttpRequest.prototype.send;
       // override the native send()
-      XMLHttpRequest.prototype.send = function(){
-          // process the callback queue
-          // the xhr instance is passed into each callback but seems pretty useless
-          // you can't tell what its destination is or call abort() without an error
-          // so only really good for logging that a request has happened
-          // I could be wrong, I hope so...
-          // EDIT: I suppose you could override the onreadystatechange handler though
+      XMLHttpRequest.prototype.send = function(){         
           for( i = 0; i < XMLHttpRequest.callbacks.length; i++ ) {
               XMLHttpRequest.callbacks[i]( this );
           }
@@ -229,7 +223,7 @@ function addXMLRequestCallback(callback){
          // console.log( xhr.responseText ); // (an empty string)
          if(xhr.responseText !== '' || xhr.responseText !== undefined )
          {
-          console.log(xhr.responseText)
+         // console.log(xhr.responseText)
          }
         });
         addXMLRequestCallback( xhr => {
