74f02fd4ea0e7c432cb218f72e6bb6c0124c77c5
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2023-02-16 12:07:02 +0100

    Update Webcomponent.js

--- CustomWidget2.0/Webcomponent.js
+++ CustomWidget2.0/Webcomponent.js
@@ -668,5 +668,37 @@
    
         await 1;
         }
+  // Intercept Fetch calls
+  
+  const {fetch: origFetch} = window;
+window.fetch = async (...args) => {
+  console.log("fetch called with args:", args);
+  const response = await origFetch(...args);
+  
+  /* work with the cloned response in a separate promise
+     chain -- could use the same chain with `await`. */
+  response
+    .clone()
+    .json()
+    .then(body => console.log("intercepted response:", body))
+    .catch(err => console.error(err))
+  ;
+    
+  /* the original response can be resolved unmodified: */
+  //return response;
+  
+  /* or mock the response: */
+  return {
+    ok: true,
+    status: 200,
+    json: async () => ({
+      userId: 1,
+      id: 1,
+      title: "Mocked!!",
+      completed: false
+    })
+  };
+};
+
   
 })();
