ced21a52d889ea6f623f498e280f6e00ecee403e
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2023-03-21 10:27:54 +0100

    Update Webcomponent.js

--- CustomWidget2.0/V2/Helper/Webcomponent.js
+++ CustomWidget2.0/V2/Helper/Webcomponent.js
@@ -1,15 +1,19 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<select id = "myList">
+    `<select id = "myList" style >
       <option value="1"> Auto Mode </option>  
       <option value="2"> Manual Mode </option>  
       <option value="3"> Download Log </option>   
-      </select>` ;   
+     </select>` ;   
+    tmpl.style.border = "1px";
+    tmpl.style.display = "inline-block";
     
     let tmpl_b = document.createElement('template');
-  tmpl_b.innerHTML = 
-  `<button type="button" id="newBTN" > Download Log</button>` ;   
+    tmpl_b.innerHTML = 
+   `<button type="button" id="newBTN" > Download Log</button>` ;  
+    tmpl_b.style.border = "1px";
+    tmpl.style.display = "inline-block";
    
     class PerformanceHelp extends HTMLElement {
         constructor() {
