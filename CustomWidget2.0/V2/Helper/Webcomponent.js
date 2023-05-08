69a17a4628ff6ac5b5f5d6a354f4d2706c31d589
PRANAVRHCP <PRANAVKANDPALRHCP@GMAIL.COM>
2023-05-08 16:55:59 +0200

    Update Webcomponent.js

--- CustomWidget2.0/V2/Helper/Webcomponent.js
+++ CustomWidget2.0/V2/Helper/Webcomponent.js
@@ -70,8 +70,8 @@ tmpl_popup.innerHTML = `
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    display: flex;
-    flex-direction: column;
+  //  display: flex;
+  //  flex-direction: column;
     align-items: center;
   }
 
